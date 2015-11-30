618a8b606c3b2c3d0a3539a38797bf1c1a906d0f
Robert Erdin <robert.erdin@gmail.com>
2015-11-30 17:32:42 +0100

    bugfix, Person had no empty constructor for morphia

--- src/back-end/src/main/java/com/whatistics/backend/model/Person.java
+++ src/back-end/src/main/java/com/whatistics/backend/model/Person.java
@@ -19,12 +19,20 @@ public class Person {
     @Reference
     private Statistics statistics = new Statistics();
 
+    public Person(){
+
+    }
+
     public Person(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
     }
 
     public Statistics getStatistics() {
