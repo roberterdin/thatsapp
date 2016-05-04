951cbf5d237d0a9b020d716f4e658f28527cc749
Robert Erdin <robert.erdin@gmail.com>
2016-05-04 15:12:26 +0200

    file had wrong intendation

--- src/back-end/src/main/java/com/whatistics/backend/WhatisticsBackend.java
+++ src/back-end/src/main/java/com/whatistics/backend/WhatisticsBackend.java
@@ -17,42 +17,42 @@ import java.util.Random;
  */
 public class WhatisticsBackend {
 
-  private static Injector injector;
+    private static Injector injector;
 
-  public static Random rand = new Random();
+    public static Random rand = new Random();
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 //    System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "DEBUG");
 
-    final Logger logger = LoggerFactory.getLogger(WhatisticsBackend.class);
+        final Logger logger = LoggerFactory.getLogger(WhatisticsBackend.class);
 
 
-    injector = Guice.createInjector(new MailModule(),
-            new ParserModule(),
-            new DataAccessLayerModule(),
-            new WhatisticsModule(),
-            new StatisticsModule());
+        injector = Guice.createInjector(new MailModule(),
+                new ParserModule(),
+                new DataAccessLayerModule(),
+                new WhatisticsModule(),
+                new StatisticsModule());
 
 
+        // start the restheart server as a process
+        // todo: make RESTHeart use configuration file from resources
+        // todo: redirect stdout or call lib directly
+        // todo: movo into seperate model so
+        try {
+            Process restProc = Runtime.getRuntime().exec("java -server -jar build/libs/lib/restheart-1.1.7.jar");
+        } catch (IOException e) {
+            logger.error("Failed starting RESTHeart Server", e);
+            e.printStackTrace();
+        }
 
-    // start the restheart server as a process
-    // todo: make RESTHeart use configuration file from resources
-    // todo: redirect stdout or call lib directly
-    try {
-      Process restProc = Runtime.getRuntime().exec("java -server -jar build/libs/lib/restheart-1.1.7.jar");
-    } catch (IOException e) {
-      logger.error("Failed starting RESTHeart Server", e);
-      e.printStackTrace();
+
+        WhatisticsService whatisticsService = injector.getInstance(WhatisticsService.class);
+        whatisticsService.start();
+
     }
 
-
-    WhatisticsService whatisticsService = injector.getInstance(WhatisticsService.class);
-    whatisticsService.start();
-
-  }
-
-  public static Injector getInjector(){
-    return injector;
-  }
+    public static Injector getInjector() {
+        return injector;
+    }
 
 }
