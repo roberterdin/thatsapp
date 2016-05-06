d7661a83c2db65ae2e666e017c95ac786d6d4f62
Robert Erdin <robert.erdin@gmail.com>
2016-05-06 14:24:51 +0200

    implemented basic version of history chart in the results/index view

--- src/front-end/app/controllers/results/index.js
+++ src/front-end/app/controllers/results/index.js
@@ -2,26 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  needs: "results",
-  parentController: Ember.computed.alias('controllers.results'),
+    needs: "results",
+    parentController: Ember.computed.alias('controllers.results'),
 
-  init: function() {
-    console.log("ResultsIndexController created");
-  },
+    init: function() {
+        console.log("ResultsIndexController created");
+    },
 
-  // TODO only dummy data inside
-  historyChart: function () {
-    return {
-      chartData: {
-        labels: ['Day1', 'Day2', 'Day3'],
-        series: [
-          [5, 4, 8],
-          [10, 2, 7],
-          [8, 3, 6]
-        ]
-      }
-    };
-  }.property('historyChart')
+    historyChart: function() {
+        var labels = [];
+        var messageAmount = [];
+        this.get('model.aggregatedHistory').forEach(function(timeInterval) {
+            labels.push(timeInterval.get('label'));
+            messageAmount.push(timeInterval.get('statistics').get('messageAmount'));
+        });
+        return {
+            chartData: {
+                labels: labels,
+                series: [
+                    messageAmount
+                ]
+            },
+            chartOptions: {
+                showPoint: false,
+                lineSmooth: false,
+            }
+        };
+    }.property('historyChart')
 
 
 });
