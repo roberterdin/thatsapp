226f5a7562a5386563bcba2e865b8cc4d2b6b922
Robert Erdin <robert.erdin@gmail.com>
2016-05-10 17:50:42 +0200

    implemented skeleton for sharing to social media and whatsapp

--- src/front-end/app/controllers/results.js
+++ src/front-end/app/controllers/results.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+    /**
+     * Returns the path of the current statistics for sharing purposes
+     */
+    currentPath: function(){
+        // http://127.0.0.1:4200/results/572c8c90ce73f23439e6b206/ladder
+        return window.location.href.split("/").slice(0,5).join("/");
+    }.property('currentPath')
 });
