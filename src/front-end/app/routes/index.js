1bb99f77b1ef260d79307a35f12f969c2e46790a
Robert Erdin <robert.erdin@gmail.com>
2016-05-11 16:30:10 +0200

    cleanup unused code

--- src/front-end/app/routes/index.js
+++ src/front-end/app/routes/index.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	setupController: function(controller, model) {
-        this._super(controller, model);
-        controller._reset();
-    }
+
 });
 
