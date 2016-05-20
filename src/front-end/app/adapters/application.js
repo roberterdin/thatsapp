32de868495f00692a2908dc1e42f4a462979548a
Robert Erdin <robert.erdin@gmail.com>
2016-05-20 12:31:33 +0200

    introduced environments for dev and prod

--- src/front-end/app/adapters/application.js
+++ src/front-end/app/adapters/application.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
+import config from '../config/environment';
 
 export default DS.RESTAdapter.extend({
   // document.location.origin --> "http://localhost:4200"
-  host: document.location.origin.split(/:\d*$/g, 1)[0] + ":8080", // --> "http://localhost:8080
+  host: document.location.origin.split(/:\d*$/g, 1)[0] + ":" + config.backEndPort, // --> "http://localhost:8080
   namespace: 'whatistics'
 });
--- src/front-end/ember-cli-build.js
+++ src/front-end/ember-cli-build.js
@@ -1,33 +1,43 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Any other options
-  });
+    var app = new EmberApp(defaults, {
+        // Any other options
+    });
 
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+    app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+    app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 
-  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-    destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
-    destDir: 'assets'
-  });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+        destDir: 'assets'
+    });
 
-  //app.import('bower_components/moment/moment.js');
-  //app.import('bower_components/modernizr/modernizr.js');
-
-  //app.import('bower_components/es6-shim/es6-shim.js');
+    //app.import('bower_components/moment/moment.js');
+    //app.import('bower_components/modernizr/modernizr.js');
 
 
-  /* vendors */
-  //app.import('vendor/bootstrap.file-input.js');
-  //app.import('vendor/file-reader.js');
-  //app.import('vendor/wordCounter.js');
-  //app.import('vendor/stopwords_de_ch.js');
-  //app.import('vendor/stopwords_de_de.js');
-  //app.import('vendor/vocabBuilder.js');
+    /* vendors */
+    //app.import('vendor/bootstrap.file-input.js');
+    //app.import('vendor/file-reader.js');
+    //app.import('vendor/wordCounter.js');
+    //app.import('vendor/stopwords_de_ch.js');
+    //app.import('vendor/stopwords_de_de.js');
+    //app.import('vendor/vocabBuilder.js');
 
-  return app.toTree();
+    return app.toTree();
 };
