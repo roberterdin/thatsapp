b258046b6c82fb354e20c8a265ef3c9a2b34c3e1
Robert Erdin <robert.erdin@gmail.com>
2016-05-24 19:10:30 +0200

    automatic back transition after error page

--- src/front-end/app/routes/error/whatsapp.js
+++ src/front-end/app/routes/error/whatsapp.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    afterModel() {
+        Ember.run.later((function() {
+            history.back();
+        }), 8000);
+    }
 });
